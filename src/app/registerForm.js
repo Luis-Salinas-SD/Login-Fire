337e5331007c9bb1af563ed7f3f5a14b04eaba01
Luis-Salinas-SD <paus.impala21@gmail.com>
2022-10-13 16:01:19 -0500

    Configuracion del toast

--- src/app/registerForm.js
+++ src/app/registerForm.js
@@ -12,7 +12,7 @@ resgisterUser.addEventListener('submit', (e) => {
     async function getUser() {
         try {
             const usuario = await createUserWithEmailAndPassword(auth, mail, pass)
-            console.log(usuario);
+            console.log(usuario.code);
 
             //Cerrar el modal una vez cumplido el try
             //en closeModal seleccionamos el modal que queremos afectar
@@ -24,7 +24,30 @@ resgisterUser.addEventListener('submit', (e) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             console.info(errorCode)
-            console.warn(errorMessage)
+            //console.warn(errorMessage)
+
+            if (errorCode === 'auth/email-already-in-use') {
+                Toastify({
+                    text: "El usuario ya exsiste",
+                    duration: 3000,
+                    newWindow: true,
+                    close: true,
+                    gravity: "bottom", // `top` or `bottom`
+                    position: "right", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                        background: " #f5b041 ",
+                    },
+                    onClick: function () { console.log('click sobre el toast') } // Callback after click
+                }).showToast();
+
+            } else if (errorCode === 'auth/invalid-email') {
+                alert('Correo invalido');
+            } else if (errorCode === 'auth/weak-password') {
+                alert('Password demasiado corto');
+            } else if (errorCode) {
+                alert('Algo salió mal')
+            }
         }
 
     }
